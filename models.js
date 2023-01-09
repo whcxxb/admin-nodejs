ae68b318845cef983332121159334bc0f42a9b17
王小白 <731896502@qq.com>
2023-01-09 18:14:17 +0800

    up

--- models.js
+++ models.js
@@ -1,13 +1,27 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', true)
-mongoose.connect('mongodb://127.0.0.1:27017/express-auth', {
-  useNewUrlParser: true
-})
+mongoose
+  .connect('mongodb://127.0.0.1:27017/expree-auth', {
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log('数据库连接成功')
+  })
+  .catch((err) => {
+    console.log('数据库连接失败')
+  })
 
 // 定义user模型
 const UserSchema = new mongoose.Schema({
   username: {
-    type: String
+    type: String,
+    // 不能重复
+    unique: true,
+    // 字符串小写
+    lowercase: true,
+    // 必填
+    required: true,
+    trim: true
   },
   password: {
     type: String
--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const { User } = require('./models')
 const app = express()
 
+// // 清空User
+// User.deleteMany({}, (err, data) => {
+//   console.log('清空User')
+// })
+
 // 解析post请求的body
 app.use(express.json())
 app.get('/api', async (req, res) => {
@@ -10,11 +15,22 @@ app.get('/api', async (req, res) => {
 
 // 注册
 app.post('/api/register', async (req, res) => {
-  const user = await User.create({
-    username: req.body.username,
-    password: req.body.password
-  })
-  res.send('ok')
+  try {
+    const user = await User.create({
+      username: req.body.username,
+      password: req.body.password
+    })
+    res.send(user)
+  } catch (error) {
+    res.status(422).send({
+      message: error.message
+    })
+  }
+})
+// 获取所有用户名
+app.get('/api/userlist', async (req, res) => {
+  const userlist = await User.find().select('username id')
+  res.send(userlist)
 })
 
 app.listen('3000', (req, res) => {
